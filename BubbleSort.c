0111d5714c0fa58458dca6709fcb4e45239005b5
Eduardoh5179 <168138997+Eduardoh5179@users.noreply.github.com>
2025-04-15 15:43:51 -0300

    Create BubbleSort.c

---
+++ BubbleSort.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define TAM 10
+
+int arr[TAM];
+
+int randomArr(){
+    int i;
+    srand(33);
+    for(i=0;i<TAM;i++){
+        arr[i] = rand() % (TAM * 10);
+    }
+    
+    return arr[TAM];
+}
+
+void printArr(){
+    int i;
+    for(i=0;i<TAM;i++){
+        printf(" %d ",arr[i]);
+    }
+}
+
+void bubbleSort(){
+    for (int i = 0; i < TAM; i++) {
+        for (int j = 0; j < TAM - 1; j++) {
+            if(arr[j]>arr[j+1]){
+                int x = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = x;
+            }
+        }
+    }
+}
+
+int main()
+{
+    randomArr();
+    bubbleSort();
+    printArr();
+}
